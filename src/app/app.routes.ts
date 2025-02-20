e48985a3af6963a83353ca664f319cd63c4d5cc6
Paul Kukowski <kukowskipaul@gmail.com>
2025-02-20 21:03:48 +0100

    Bootstrap app & config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { OverviewComponent } from './overview/overview.component';
+
+export const routes: Routes = [
+  { path: '', component: OverviewComponent },
+  {
+    path: 'players/:id',
+    component: PlayerDetailComponent,
+  },
+];
